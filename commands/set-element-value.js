cf0bc5437020ae7c0d1ca3d156245e20bed49c5c
Allen Kim <allenhwkim@gmail.com>
2017-01-27 20:46:59 -0500

    addes set element value

--- commands/set-element-value.js
+++ commands/set-element-value.js
@@ -3,16 +3,19 @@ var seleniumWebTestDriver = require('../src/selenium-web-test-driver');
 const RE_STR  = '["]?([^\"]+)["]?'; // e.g. foo.bar, "foo.bar", or "foo bar"
 
 module.exports = {
-  name: 'show element',
-  help: 'show element <selector>',
-  regExp: new RegExp(`^show element ${RE_STR}$`),
+  name: 'set element value',
+  help: 'set element <selector> value <value>',
+  regExp: new RegExp(`^set element ${RE_STR} value ${RE_STR}`),
   func:
-    /** must return a Promise, so that it can be chained with next command*/
-    function(selector) {
+    function(selector, value) {
       return seleniumWebTestDriver.findBy('css', selector)
         .then(element => {
+          // console.log('element', element.constructor.name)
+          seleniumWebTestDriver.driver.executeScript(
+            `arguments[0].value = '${value}'`, element
+          );
           seleniumWebTestDriver.lastFoundElement = element;
-          element.getOuterHtml().then(html => console.log(html));
         });
     }
 };
+
