47dc4c1bca541d4b5ecc9a15348322171b2b7f0e
Allen Kim <allenhwkim@gmail.com>
2017-01-29 14:04:26 -0500

    0.2.13

--- commands/set-element-value.js
+++ commands/set-element-value.js
@@ -1,20 +1,25 @@
 'use strict';
 var seleniumWebTestDriver = require('../src/selenium-web-test-driver');
 const RE_STR  = '["]?([^\"]+)["]?'; // e.g. foo.bar, "foo.bar", or "foo bar"
-const RE_STR_WITH_QUOTE = '[\'"]?([\\s\\S]*?)[\'"]?'; //e.g. 'foo bar', "foo bar"
+const RE_STR_WITH_QUOTE = '[\'"]?([\\s\\S]*)[\'"]?'; //e.g. 'foo bar', "foo bar"
 
 module.exports = {
-  name: 'verify element value',
-  help: 'verify element <selector> value "<string>"',
-  regExp: new RegExp(`^verify element ${RE_STR} value [is ]*${RE_STR_WITH_QUOTE}$`),
+  name: 'set element value',
+  help: 'set element <selector> value "<string>"',
+  regExp: new RegExp(`^set element ${RE_STR} value [as ]*${RE_STR_WITH_QUOTE}$`),
   /** must return a Promise, so that it can be chained with next command*/
   func: function(selector, value) {
-    value = value || '';
     let cssSelector = `${selector}, [placeholder='${selector}']`;
+    value = value || "";
     return seleniumWebTestDriver.driver.wait( function() {
       return seleniumWebTestDriver.driver.findElement({css: cssSelector})
-        .getAttribute("value")
-        .then( val => val == value);
+        .then(element => {
+          return seleniumWebTestDriver.driver.executeScript(`
+            arguments[0].setAttribute('value', '${value}');
+            arguments[0].dispatchEvent(new Event('change'));
+            return true;
+          `, element);
+        })
     }, seleniumWebTestDriver.config.timeout);
   }
 };
--- commands/verify-element-value-match.js
+++ commands/verify-element-value-match.js
@@ -1,20 +1,19 @@
 'use strict';
 var seleniumWebTestDriver = require('../src/selenium-web-test-driver');
 const RE_STR  = '["]?([^\"]+)["]?'; // e.g. foo.bar, "foo.bar", or "foo bar"
-const RE_STR_WITH_QUOTE = '[\'"]?([\\s\\S]*?)[\'"]?'; //e.g. 'foo bar', "foo bar"
+const RE_STR_WITH_QUOTE = '[\'"]([\\s\\S]+)[\'"]'; //e.g. 'foo bar', "foo bar"
 
 module.exports = {
-  name: 'verify element value',
-  help: 'verify element <selector> value "<string>"',
-  regExp: new RegExp(`^verify element ${RE_STR} value [is ]*${RE_STR_WITH_QUOTE}$`),
+  name: 'verify element value match',
+  help: 'verify element <selector> value matches "<string>"',
+  regExp: new RegExp(`^verify element ${RE_STR} value matches [to ]*${RE_STR_WITH_QUOTE}`),
   /** must return a Promise, so that it can be chained with next command*/
-  func: function(selector, value) {
-    value = value || '';
+  func: function(selector, valueRE) {
     let cssSelector = `${selector}, [placeholder='${selector}']`;
     return seleniumWebTestDriver.driver.wait( function() {
       return seleniumWebTestDriver.driver.findElement({css: cssSelector})
         .getAttribute("value")
-        .then( val => val == value);
+        .then( val => val.match(valueRE));
     }, seleniumWebTestDriver.config.timeout);
   }
 };
