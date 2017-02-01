2989823b6e118532c0e362412fdafd19d2e9c28a
Allen Kim <allenhwkim@gmail.com>
2017-02-01 08:02:04 -0500

    updated

--- commands/select-option.js
+++ commands/select-option.js
@@ -1,18 +1,18 @@
 'use strict';
+let SeleniumWebDriver = require('selenium-webdriver');
 var seleniumWebTestDriver = require('../src/selenium-web-test-driver');
-const RE_STR  = '["]?([^\"]+)["]?'; // e.g. foo.bar, "foo.bar", or "foo bar"
+const RE_STR  = '["]?([^\"]+)["]?'; // e.g. foo.bar, "foo.bar"
 const RE_STR_WITH_QUOTE = '[\'"]([\\s\\S]+)[\'"]'; //e.g. 'foo bar', "foo bar"
-
 /**
  * e.g. enter text 'foo bar' into .foo.bar
  */
 module.exports = {
-  name: 'enter text',
-  help: 'enter text <string> into <selector>',
-  regExp: new RegExp(`^enter text ${RE_STR_WITH_QUOTE} into ${RE_STR}`),
+  name: 'select option',
+  help: 'select "<text>" from <selector>',
+  regExp: new RegExp(`^select ${RE_STR_WITH_QUOTE} from ${RE_STR}`),
   func: /** must return a Promise, so that it can be chained with next command*/
-    function(text, selector) {
+    function(string, selector) {
       return seleniumWebTestDriver.findBy('css', selector)
-        .then(element =>  element.sendKeys(text));
+        .then(element => element.sendKeys(string))
     }
 };
